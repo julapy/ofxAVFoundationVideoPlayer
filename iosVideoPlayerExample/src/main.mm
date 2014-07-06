b81e37297a8a28a960b183d429e0daaba34e7ca7
lukasz karluk <julapy@googlemail.com>
2014-07-06 18:41:58 +1000

    added osx example which is broken until next commit.

--- iosVideoPlayerExample/src/main.mm
+++ iosVideoPlayerExample/src/main.mm
@@ -1,7 +1,11 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include "ofAppiOSWindow.h"
 
-int main(){
-	ofSetupOpenGL(1024,768, OF_FULLSCREEN);			// <-------- setup the GL context
+int main() {
+    ofAppiOSWindow * window = new ofAppiOSWindow();
+    window->enableRendererES2();
+    
+	ofSetupOpenGL(window, 1024,768, OF_FULLSCREEN);
 	ofRunApp(new ofApp);
 }
--- iosVideoPlayerExample/src/ofApp.h
+++ iosVideoPlayerExample/src/ofApp.h
@@ -6,38 +6,38 @@
 #include "ofxAVFoundationVideoPlayer.h"
 
 class ofApp : public ofxiOSApp{
-	
-	public:
-		
-		void setup();
-		void update();
-		void draw();
-        void exit();
-    
-        void touchDown(ofTouchEventArgs & touch);
-        void touchMoved(ofTouchEventArgs & touch);
-        void touchUp(ofTouchEventArgs & touch);
-        void touchDoubleTap(ofTouchEventArgs & touch);
-        void touchCancelled(ofTouchEventArgs & touch);
-	
-        void lostFocus();
-        void gotFocus();
-        void gotMemoryWarning();
-        void deviceOrientationChanged(int newOrientation);
-    
-        void playPressed();
-        void pausePressed();
-        void scrubBegin();
-        void scrubToPosition(float position);
-        void scrubEnd();
-        void loadPressed();
-        void unloadPressed();
-        void loopOnPressed();
-        void loopOffPressed();
-        void nativeOnPressed();
-        void nativeOffPressed();
-        void muteOnPressed();
-        void muteOffPressed();
 
-        ofxAVFoundationVideoPlayer video;
+public:
+    
+    void setup();
+    void update();
+    void draw();
+    void exit();
+    
+    void touchDown(ofTouchEventArgs & touch);
+    void touchMoved(ofTouchEventArgs & touch);
+    void touchUp(ofTouchEventArgs & touch);
+    void touchDoubleTap(ofTouchEventArgs & touch);
+    void touchCancelled(ofTouchEventArgs & touch);
+	
+    void lostFocus();
+    void gotFocus();
+    void gotMemoryWarning();
+    void deviceOrientationChanged(int newOrientation);
+    
+    void playPressed();
+    void pausePressed();
+    void scrubBegin();
+    void scrubToPosition(float position);
+    void scrubEnd();
+    void loadPressed();
+    void unloadPressed();
+    void loopOnPressed();
+    void loopOffPressed();
+    void nativeOnPressed();
+    void nativeOffPressed();
+    void muteOnPressed();
+    void muteOffPressed();
+    
+    ofxAVFoundationVideoPlayer video;
 };
